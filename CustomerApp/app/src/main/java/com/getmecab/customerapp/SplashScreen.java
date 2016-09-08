1a360d0a4cad70553a9de4a0a62b1d54b96fce94
anuj <anuj@roboticwares.com>
2016-09-09 02:48:34 +0530

    initial commit

--- CustomerApp/app/src/main/java/com/getmecab/customerapp/SplashScreen.java
+++ CustomerApp/app/src/main/java/com/getmecab/customerapp/SplashScreen.java
@@ -1,13 +1,29 @@
 package com.getmecab.customerapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
